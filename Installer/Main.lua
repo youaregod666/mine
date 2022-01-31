92a37d6e0f832dfecae5e89dd4bcd6c6bffb6ee6
youaregod666 <94928206+youaregod666@users.noreply.github.com>
2022-01-31 21:34:19 +0100

    Update Main.lua

--- Installer/Main.lua
+++ Installer/Main.lua
@@ -17,7 +17,7 @@ local EEPROMProxy, internetProxy, GPUProxy =
 GPUProxy.bind(getComponentAddress("screen"))
 local screenWidth, screenHeight = GPUProxy.getResolution()
 
-local repositoryURL = "https://raw.githubusercontent.com/IgorTimofeev/MineOS/master/"
+local repositoryURL = "https://raw.githubusercontent.com/youaregod666/mine/master/"
 local installerURL = "Installer/"
 local EFIURL = "EFI/Minified.lua"
 
