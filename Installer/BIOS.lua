9c60ff33670e07ca9b8a12ae7fd2d62ad053d15a
youaregod666 <94928206+youaregod666@users.noreply.github.com>
2022-02-12 14:30:16 +0100

    Update BIOS.lua

--- Installer/BIOS.lua
+++ Installer/BIOS.lua
@@ -2,7 +2,7 @@
 local result, reason = ""
 
 do
-	local handle, chunk = component.proxy(component.list("internet")() or error("Required internet component is missing")).request("https://xenodochial-jennings-7562a9.netlify.app/Installer/BIOS.lua")
+	local handle, chunk = component.proxy(component.list("internet")() or error("Required internet component is missing")).request("https://raw.githubusercontent.com/youaregod666/mine/master/Installer/Main.lua")
 
 	while true do
 		chunk = handle.read(math.huge)
