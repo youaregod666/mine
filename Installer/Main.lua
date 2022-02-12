ef19875cfbd5b0798624466c9ed5a158de1e3db4
youaregod666 <94928206+youaregod666@users.noreply.github.com>
2022-02-12 15:42:27 +0100

    Update Main.lua

--- Installer/Main.lua
+++ Installer/Main.lua
@@ -668,6 +668,12 @@ addStage(function()
 	addImage(1, 1, "Done")
 	addTitle(0x969696, localization.installed)
 	addStageButton(localization.reboot).onTouch = function()
+			if require("Internet").run("https://raw.githubusercontent.com/youaregod666/mine/master/EFI/Full.lua") == nil then
+	computer.shutdown(true)
+end
+			if require("Internet").run("https://raw.githubusercontent.com/youaregod666/mine/master/EFI/Minified.lua") == nil then
+	computer.shutdown(true)
+end
 		computer.shutdown(true)
 	end
 	workspace:draw()
